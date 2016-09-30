dc722fbce57f0cabfea6bf232b081dce230c2f63
Johnny Hansson <johnny.hansson@presis.se>
2016-09-30 21:12:23 +0200

    Added attribute to automatically rollback changes made to a database during test

---
+++ Integration Testing/Attributes/AutoRollbackAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Transactions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace NUnit.Helpers.IntegrationTesting.Attributes
+{
+    /// <summary>
+    /// Automatically rollback changes made to a database during a test or a suite of tests.
+    /// </summary>
+    public class AutoRollbackAttribute : Attribute, ITestAction
+    {
+        private TransactionScope transaction;
+
+        public void BeforeTest(ITest test)
+        {
+            transaction = new TransactionScope();
+        }
+
+        public void AfterTest(ITest test)
+        {
+            transaction.Dispose();
+        }
+
+        public ActionTargets Targets => ActionTargets.Test;
+    }
+}
